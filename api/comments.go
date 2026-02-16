881a7609a52153fb250f1fed978c7dd40ceba4a0
Angus-Warman <anguswarman@gmail.com>
2026-02-17 07:05:20 +0800

    Comments using JSONL and UUID

--- api/comments.go
+++ api/comments.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"crypto/rand"
+	"encoding/binary"
 	"encoding/json"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -12,15 +16,16 @@ import (
 )
 
 type Comment struct {
+	ID      string `json:"id"`
 	Name    string `json:"name"`
 	Body    string `json:"body"`
 	Created string `json:"created"`
 }
 
 var (
-	dataFile = "comments.json"
-	mu       sync.Mutex
-	tmpl     = template.Must(template.New("comments").Parse(fragmentTemplate))
+	commentsFile = "comments.jsonl"
+	mu           sync.RWMutex
+	tmpl         = template.Must(template.New("comments").Parse(fragmentTemplate))
 )
 
 func main() {
@@ -47,31 +52,31 @@ func postComment(w http.ResponseWriter, r *http.Request) {
 	body := strings.TrimSpace(r.FormValue("body"))
 	honeypot := r.FormValue("website")
 
-	if honeypot == "" &&
-		name != "" &&
-		body != "" &&
-		len(name) <= 80 &&
-		len(body) <= 1000 {
-
-		mu.Lock()
-		comments := loadComments()
-		newComment := Comment{
-			Name:    name,
-			Body:    body,
-			Created: time.Now().Format("2006-01-02 15:04"),
-		}
-		comments = append(comments, newComment)
-		saveComments(comments)
-		mu.Unlock()
+	if honeypot != "" || name == "" || body == "" || len(name) > 80 || len(body) > 1000 {
+		return
 	}
+
+	mu.Lock()
+	newComment := Comment{
+		ID:      newUUID(),
+		Name:    name,
+		Body:    body,
+		Created: time.Now().Format("2006-01-02 15:04"),
+	}
+	saveComment(newComment)
+	mu.Unlock()
 
 	renderCommentSection(w)
 }
 
 func renderCommentSection(w http.ResponseWriter) {
-	mu.Lock()
-	comments := loadComments()
-	mu.Unlock()
+	mu.RLock()
+	comments, err := loadComments()
+	mu.RUnlock()
+
+	if err != nil {
+		http.Error(w, "comment load error", http.StatusInternalServerError)
+	}
 
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if err := tmpl.Execute(w, comments); err != nil {
@@ -80,24 +85,85 @@ func renderCommentSection(w http.ResponseWriter) {
 }
 
 func ensureDataFile() {
-	if _, err := os.Stat(dataFile); os.IsNotExist(err) {
-		os.WriteFile(dataFile, []byte("[]"), 0644)
+	if _, err := os.Stat(commentsFile); os.IsNotExist(err) {
+		os.WriteFile(commentsFile, []byte(""), 0644)
 	}
 }
 
-func loadComments() []Comment {
-	data, err := os.ReadFile(dataFile)
+func loadComments() ([]Comment, error) {
+	file, err := os.Open(commentsFile)
 	if err != nil {
-		return []Comment{}
+		return nil, err
 	}
-	var comments []Comment
-	json.Unmarshal(data, &comments)
-	return comments
+	defer file.Close()
+
+	comments := []Comment{}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Bytes()
+		var comment Comment
+		if err := json.Unmarshal(line, &comment); err != nil {
+			return nil, err
+		}
+		comments = append(comments, comment)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return comments, nil
 }
 
-func saveComments(comments []Comment) {
-	data, _ := json.MarshalIndent(comments, "", "  ")
-	os.WriteFile(dataFile, data, 0644)
+func saveComment(comment Comment) error {
+	data, err := json.Marshal(comment)
+
+	if err != nil {
+		return err
+	}
+
+	f, err := os.OpenFile(commentsFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	if err != nil {
+		return err
+	}
+
+	f.Write(data)
+	f.Write([]byte{'\n'})
+
+	if err := f.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func newUUID() string {
+	var b [16]byte
+
+	// Unix time in milliseconds, 48 bits
+	ms := uint64(time.Now().UnixMilli())
+	binary.BigEndian.PutUint64(b[0:8], ms<<16)
+
+	// 10 bits of randomness
+	if _, err := rand.Read(b[6:]); err != nil {
+		return ""
+	}
+
+	// Set V7
+	b[6] = (b[6] & 0x0f) | 0x70
+
+	// Set variant RFC 4122
+	b[8] = (b[8] & 0x3f) | 0x80
+
+	return fmt.Sprintf("%08x-%04x-%04x-%04x-%012x",
+		b[0:4],
+		b[4:6],
+		b[6:8],
+		b[8:10],
+		b[10:16],
+	)
 }
 
 const fragmentTemplate = `
